07a731b25792fe517cbb0b175ceb0dd6e1aaf440
unknown <bluu@SFOWLP-FP44H12.ad.businesswire.com>
2017-06-19 12:23:48 -0700

    add header to CORS

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ app.use(bodyParser.json());
 
 // Enable CORS from client side
 app.use(function(req, res, next) {  
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Credentials');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
