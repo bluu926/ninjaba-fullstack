626d460e9fb5224ae6987ad6a3dbb8148c08f41d
bluu926 <bluumail@gmail.com>
2017-06-19 21:40:36 -0700

    debugging foundPlayer

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -121,7 +121,7 @@ exports.addPlayer = function(req, res, next) {
 			return res.status(422).json({ error: 'No player was found.' });
 		}
 
-		console.log("************************* " + foundPlayer);
+		console.log("************************* " + playerId + " " + username + " : " + foundPlayer);
 
 		// if player found, change it is a Free Agent
 		if (foundPlayer.owner != '--free agent--') {
