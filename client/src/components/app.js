1156b92d003b83e40af778d75b955e5add6d75ec
bluu926 <bluumail@gmail.com>
2017-06-21 17:02:33 -0700

    heroku

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,31 +1,10 @@
 import React, { Component } from 'react';
-import {Navbar, NavbarHeader, NavbarItems, Item, NavbarDropdown, DropdownMenu} from 'react-bootstrap-navbar';
-
-const dropdownItems = [
-    {href: '#', name: 'Placeholder'},
-    {href: '#', name: 'Placeholder'},
-    {href: '#', name: 'Placeholder'},
-];
-
-const navbar = (
-	<Navbar>
-	    <NavbarHeader href="homepage.html" name="Website Name"/>
-	    <NavbarItems>
-	        <Item link="/#/dashboard" title="Players" />
-	        <Item link="/#/login" title="Login" />
-	        <NavbarDropdown name="Ignore">
-	              <DropdownMenu menuItems={dropdownItems}/>
-	        </NavbarDropdown>
-	    </NavbarItems>
-	</Navbar>
-);
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<p>~~~~~</p>
-				{navbar}				
+				<p>Header here</p>
 
 				<div className="container">
 					{this.props.children}
