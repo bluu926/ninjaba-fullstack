f4b14780148a315e23fa3e6ba1b059e7f256bf67
bluu926 <bluumail@gmail.com>
2017-06-21 17:20:04 -0700

    hero

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import {Navbar, NavbarHeader, NavbarItems, Item, NavbarDropdown, DropdownMenu} from 'react-bootstrap-navbar';
+
+const dropdownItems = [
+    {href: '#', name: 'Placeholder'},
+    {href: '#', name: 'Placeholder'},
+    {href: '#', name: 'Placeholder'},
+];
+ 
+const NavBar = (
+    <Navbar>
+        <NavbarHeader href="/" name="Website Name"/>
+        <NavbarItems>
+            <Item link="/#/dashboard" title="Players" />
+            <NavbarDropdown name="Placeholder">
+                  <DropdownMenu menuItems={dropdownItems}/>
+            </NavbarDropdown>
+        </NavbarItems>
+    </Navbar>
+);
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<p>Header here</p>
+				<NavBar />
 
 				<div className="container">
 					{this.props.children}
