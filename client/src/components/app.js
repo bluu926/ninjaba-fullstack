a811de97260b7d654c6d4eedf0072972db695578
bluu926 <bluumail@gmail.com>
2017-06-21 21:56:42 -0700

    heroku

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -6,29 +6,12 @@ const dropdownItems = [
     {href: '#', name: 'Placeholder'},
     {href: '#', name: 'Placeholder'},
 ];
- 
-var NavBar = React.createClass({
-    render() { 
-        return (
-            <Navbar>
-                <NavbarHeader href="/" name="Website Name"/>
-                <NavbarItems>
-                    <Item link="/#/dashboard" title="Players" />
-                    <NavbarDropdown name="Placeholder">
-                          <DropdownMenu menuItems={dropdownItems}/>
-                    </NavbarDropdown>
-                </NavbarItems>
-            </Navbar>
-        )
-	}
-});
 
 var App = React.createClass({
 	render() {
 		return (
 			<div>
 				<p>Header here</p>
-				<NavBar />
 
 				<div className="container">
 					{this.props.children}
