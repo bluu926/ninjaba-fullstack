4185078e1f4bba0bcf967db0fa2dfb4e1b961e6e
bluu926 <bluumail@gmail.com>
2017-06-21 23:33:09 -0700

    navbar

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -3,46 +3,32 @@ import React, { Component } from 'react';
 class HeaderNav extends Component {
 	render() {
 		return (
-		  <nav className="navbar navbar-default">
-			  <div className="container-fluid">
+		  <nav className='navbar navbar-default'>
+			  <div className='container-fluid'>
 			    
-			    <div className="navbar-header">
-			      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-			        <span className="sr-only">Toggle navigation</span>
-			        <span className="icon-bar"></span>
-			        <span className="icon-bar"></span>
-			        <span className="icon-bar"></span>
+			    <div className='navbar-header'>
+			      <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
+			        <span className='sr-only'>Toggle navigation</span>
+			        <span className='icon-bar'></span>
+			        <span className='icon-bar'></span>
+			        <span className='icon-bar'></span>
 			      </button>
-			      <a className="navbar-brand" href="#">Ninja Basketball</a>
+			      <a className='navbar-brand' href='/#/'>Ninja Basketball</a>
 			    </div>
 
-			    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			      <ul className="nav navbar-nav">
-			        <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-			        <li><a href="#">Link</a></li>
-			        <li className="dropdown">
-			          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-			          <ul className="dropdown-menu">
-			            <li><a href="#">Action</a></li>
-			            <li><a href="#">Another action</a></li>
-			            <li><a href="#">Something else here</a></li>
-			            <li role="separator" className="divider"></li>
-			            <li><a href="#">Separated link</a></li>
-			            <li role="separator" className="divider"></li>
-			            <li><a href="#">One more separated link</a></li>
-			          </ul>
-			        </li>
+			    <div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+			      <ul className='nav navbar-nav'>
+			        <li><a href='/#/dashboard'>Players</a></li>
 			      </ul>
-			      <ul className="nav navbar-nav navbar-right">
-			        <li><a href="#">Link</a></li>
-			        <li className="dropdown">
-			          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-			          <ul className="dropdown-menu">
-			            <li><a href="#">Action</a></li>
-			            <li><a href="#">Another action</a></li>
-			            <li><a href="#">Something else here</a></li>
-			            <li role="separator" className="divider"></li>
-			            <li><a href="#">Separated link</a></li>
+			      <ul className='nav navbar-nav navbar-right'>
+			        <li><a href='#'>Link</a></li>
+			        <li className='dropdown'>
+			          <a href='#' className='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Dropdown <span className='caret'></span></a>
+			          <ul className='dropdown-menu'>
+			            <li><a href='#'>Action</a></li>
+			            <li><a href='#'>Another action</a></li>
+			            <li role='separator' className='divider'></li>
+			            <li><a href='#'>Separated link</a></li>
 			          </ul>
 			        </li>
 			      </ul>
