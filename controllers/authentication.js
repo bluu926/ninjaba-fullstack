503b790bc18ed9cdc2e2d9ad6729661ae8dd887c
unknown <bluu@SFOWLP-FP44H12.ad.businesswire.com>
2017-06-26 13:48:04 -0700

    heroku

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -127,7 +127,7 @@ exports.addPlayer = function(req, res, next) {
 			return res.status(422).json({ error: 'Player is not a free agent.' });
 		}
 
-		const numOfPlayers = User.findOne({ username: username}).count();
+		const numOfPlayers = Player.find({ owner: username}).count();
 
 		if(!numOfPlayers || numOfPlayers >= 5) {
 			return res.status(422).json({ error: 'Player limit reached.'});
