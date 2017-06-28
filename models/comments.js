00daa1ed440fcd7d68e7b56d8d60bb9787910197
unknown <bluu@SFOWLP-FP44H12.ad.businesswire.com>
2017-06-28 14:26:50 -0700

    heroku

--- models/comments.js
+++ models/comments.js
@@ -7,8 +7,11 @@ var Schema = mongoose.Schema;
 //create new instance of the mongoose.schema. the schema takes an 
 //object that shows the shape of your database entries.
 var CommentsSchema = new Schema({
- author: String,
- text: String
+	author: String,
+	text: String
+},
+{
+  	timestamps: true
 });
 
 //export our module to use in server.js
--- router.js
+++ router.js
@@ -59,32 +59,27 @@ module.exports = function(app) {
 		});
 	});
 
-	//adding the /comments route to our /api router
-	authRoutes.route('/comments')
-		//retrieve all comments from the database
-		.get(function(req, res) {
-			//looks at our Comment Schema
-			Comment.find(function(err, comments) {
-				if (err)
-					res.send(err);
-
-				//responds with a json object of our database comments.
-				res.json(comments)
-			});
-		})
-		//post new comment to the database
-		.post(function(req, res) {
-			var comment = new Comment();
-			//body parser lets us use the req.body
-			comment.author = req.body.author;
-			comment.text = req.body.text;
-			comment.save(function(err) {
-
+	authRoutes.get('/comments', (req, res) => {
+	    Comment.find(function(err, comments) {
 			if (err)
 				res.send(err);
 
-			res.json({ message: 'Comment successfully added!' });
+			//responds with a json object of our database comments.
+			res.json(comments)
 		});
+	});
+
+	authRoutes.post('/comments', (req, res) => {
+	    var comment = new Comment();
+		//body parser lets us use the req.body
+		comment.author = req.body.author;
+		comment.text = req.body.text;
+		comment.save(function(err) {
+
+		if (err)
+			res.send(err);
+
+		res.json({ message: 'Comment successfully added!' });
 	});
 
 	// Test protected route
