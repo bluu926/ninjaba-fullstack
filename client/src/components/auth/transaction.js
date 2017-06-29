c27ee4b71bba09b4868480ab1abb402060eb0d9e
unknown <bluu@SFOWLP-FP44H12.ad.businesswire.com>
2017-06-29 11:45:13 -0700

    heroku

--- client/src/components/auth/transaction.js
+++ client/src/components/auth/transaction.js
@@ -8,13 +8,16 @@ function dateFormatter(cell, row) {
 
 //	return date.toTimeString();
 
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+	var year = date.getFullYear();
 	var hours = date.getHours();
 	var minutes = date.getMinutes();
 	var ampm = hours >= 12 ? 'pm' : 'am';
 	hours = hours % 12;
 	hours = hours ? hours : 12; // the hour '0' should be '12'
 	minutes = minutes < 10 ? '0'+minutes : minutes;
-	var strTime = hours + ':' + minutes + ' ' + ampm;
+	var strTime = month + '/' + day + '/' + year + ' ' + hours + ':' + minutes + ' ' + ampm;
 	return strTime;
 }
 
